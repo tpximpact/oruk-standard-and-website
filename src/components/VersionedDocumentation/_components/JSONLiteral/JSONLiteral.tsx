c5e2d3baa16dfec71ff08e4cc0cb7f6773e732bd
Jeff Cumpsty <jeff@cumpsty.co.uk>
2026-02-03 14:20:34 +0100

    refactor: remove unused updateFrontMatter script and related functionality

--- src/components/VersionedDocumentation/_components/JSONLiteral/JSONLiteral.tsx
+++ src/components/VersionedDocumentation/_components/JSONLiteral/JSONLiteral.tsx
@@ -8,31 +8,32 @@ interface JSONLiteralProps {
 }
 
 export const JSONLiteral = ({ data }: JSONLiteralProps) => (
-  <ReactJsonView
-    name={null}
-    collapsed={2}
-    collapseStringsAfterLength={48}
-    enableClipboard={false}
-    displayDataTypes={false}
-    className={styles.jsonContainer}
-    theme={{
-      base00: '#000',
-      base01: '#fff',
-      base02: '#555', // indent brackets - doesnt need contrast
-      base03: '#fff',
-      base04: '#999', // counts
-      base05: '#fff',
-      base06: '#fff',
-      base07: '#8bb4f7', // keys
-      base08: '#fff',
-      base09: '#fff',
-      base0A: '#fff',
-      base0B: '#fff',
-      base0C: '#fff',
-      base0D: '#fff', // twirlies
-      base0E: '#fff',
-      base0F: '#fff'
-    }}
-    src={data}
-  />
+  <div className={styles.jsonContainer}>
+    <ReactJsonView
+      name={null}
+      collapsed={2}
+      collapseStringsAfterLength={48}
+      enableClipboard={false}
+      displayDataTypes={false}
+      theme={{
+        base00: '#000',
+        base01: '#fff',
+        base02: '#555', // indent brackets - doesnt need contrast
+        base03: '#fff',
+        base04: '#999', // counts
+        base05: '#fff',
+        base06: '#fff',
+        base07: '#8bb4f7', // keys
+        base08: '#fff',
+        base09: '#fff',
+        base0A: '#fff',
+        base0B: '#fff',
+        base0C: '#fff',
+        base0D: '#fff', // twirlies
+        base0E: '#fff',
+        base0F: '#fff'
+      }}
+      src={data}
+    />
+  </div>
 )
